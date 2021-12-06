e316f796fbb5fdcfd4cf1c581a2901e59d04167b
yagince <straitwalk@gmail.com>
2021-12-06 11:44:15 +0900

    refactoring example

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,16 +3,33 @@ use google_apis_ex::storage::client::Client;
 
 #[derive(Parser)]
 struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommands,
+}
+
+#[derive(Parser)]
+enum SubCommands {
+    Storage(Storage),
+}
+
+#[derive(Parser)]
+struct Storage {
     #[clap(short, long)]
     bucket: String,
     #[clap(short, long)]
     object_id: String,
 }
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = Opts::parse();
 
-    let client = Client::new().await?;
-    dbg!(client.object(&opts.bucket, &opts.object_id).await?);
+    match opts.subcmd {
+        SubCommands::Storage(opts) => {
+            let client = Client::new().await?;
+            dbg!(client.object(&opts.bucket, &opts.object_id).await?);
+        }
+    }
+
     Ok(())
 }
